97b1f3935b71cc7da7e0f450857c4efdf959dfb9
nick-dolf <nickdolf@gmail.com>
2022-06-09 12:56:14 -0500

    added staging cookies

--- routes/staging.js
+++ routes/staging.js
@@ -3,31 +3,41 @@ const cookieSession= require('cookie-session')
 const express = require('express')
 const router = express.Router()
 
+router.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  maxAge: 30000
+}))
+
+router.get('/login', (req, res) => {
+  res.render('login', {warning: req.session.original})
+})
+
+router.post('/login', (req, res) => {
+  if ( req.body.user.toLowerCase() == 'staging') {
+    req.session.user = 'staging'
+    if (req.session.original) {
+      res.redirect('.' + req.session.original)
+    } else {
+      res.redirect('./test.html' )
+    }
+
+  } else {
+    res.render('login', {warning: 'Please type staging into input box'})
+  }
+})
+
+router.use((req, res, next) => {
+  if (req.session.user == 'staging') {
+    next()
+  } else {
+    req.session.original = req.url
+    res.redirect('login')
+  }
+})
+
+
 router.use(express.static('../staging'))
 
+
 module.exports = router
-// app.use(cookieSession({
-//   name: 'session',
-//   keys: ['key1', 'key2']
-// }))
-//
-// app.get('/', (req, res) => {
-//   if (req.session.user == 'staging') {
-//     res.send('valid cookie')
-//   } else {
-//     res.status(401).end()
-//   }
-// })
-//
-// app.get('/login', (req, res) => {
-//   res.render('login', {warning: ''})
-// })
-//
-// app.post('/login', (req, res) => {
-//   if ( req.body.user.toLowerCase() == 'staging') {
-//     req.session.user = 'staging'
-//     res.redirect('/')
-//   } else {
-//     res.render('login', {warning: 'Please type staging into input box'})
-//   }
-// })
